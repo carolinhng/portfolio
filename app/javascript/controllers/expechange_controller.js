cee06d621b35bb73a5bf3d28572f415cad7f925e
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2024-01-19 16:35:16 +0100

    experience

--- app/javascript/controllers/expechange_controller.js
+++ app/javascript/controllers/expechange_controller.js
@@ -3,20 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="expechange"
 export default class extends Controller {
 
-  static targets = ["elementOne", "elementTwo", "elementThree", "elementFour", "elementFive", "resume"]
+  static targets = ["elementOne", "elementTwo", "elementThree", "elementFour", "elementFive", "resumeOne"]
 
   connect() {
   }
 
-  classChange() {
-    console.log(this.elementOneTarget)
-    console.log(this.elementTwoTarget)
-    console.log(this.elementThreeTarget)
-    console.log(this.elementFourTarget)
-    console.log(this.elementFiveTarget)
-    console.log(this.elementResumeTarget)
-
-    if 
+  classChangeOne() {
+    if (this.elementOneTarget.classList.contains("click-on"))
 
   }
 
