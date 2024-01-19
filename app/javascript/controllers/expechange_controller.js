4329a05133135b0b7fa693bbc7885e4f936fb24b
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2024-01-19 16:02:19 +0100

    stimulus

--- app/javascript/controllers/expechange_controller.js
+++ app/javascript/controllers/expechange_controller.js
@@ -3,29 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="expechange"
 export default class extends Controller {
 
-  static targets = ["elements", "resume"]
+  static targets = ["elementOne", "elementTwo", "elementThree", "elementFour", "elementFive", "resume"]
 
   connect() {
-    console.log(this.elementsTargets)
   }
 
   classChange() {
-    console.log(this.elementsTargets)
+    console.log(this.elementOneTarget)
+    console.log(this.elementTwoTarget)
+    console.log(this.elementThreeTarget)
+    console.log(this.elementFourTarget)
+    console.log(this.elementFiveTarget)
+    console.log(this.elementResumeTarget)
 
-    this.elementsTargets.forEach(target => {
-      if (target.classList.contains("click-on")) {
-        this.resumeTarget.innerHTML = `<%= render "sections/banner" %>`;
-        // target.classList.remove("btn-danger");
-        // target.classList.add("btn-success")
-      }
-      else {
-        target.innerHTML = `<i class="fa-solid fa-bolt"></i> Rendre mon jeu disponible`
-        target.classList.remove("btn-success")
-        target.classList.add("btn-danger")
-      }
-    });
+    if 
+
   }
-
-
 
 }
