97f24816e19659e30ff7a0868da838fda2e873da
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2024-01-23 15:49:32 +0100

    stimulus suite

--- app/javascript/controllers/expechange_controller.js
+++ app/javascript/controllers/expechange_controller.js
@@ -3,14 +3,25 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="expechange"
 export default class extends Controller {
 
-  static targets = ["elementOne", "elementTwo", "elementThree", "elementFour", "elementFive", "resumeOne"]
+  static targets = ["company"]
 
   connect() {
-  }
-
-  classChangeOne() {
-    if (this.elementOneTarget.classList.contains("click-on"))
 
   }
 
+  classChange(event) {
+    // Log the event details
+    console.log(event);
+
+    // Iterate over company targets
+    this.companyTargets.forEach(target => {
+      if (target === event.currentTarget) {
+        target.classList.add("click-on");
+        target.classList.remove("click-off");
+      } else {
+        target.classList.remove("click-on");
+        target.classList.add("click-off");
+      }
+    });
+  }
 }
