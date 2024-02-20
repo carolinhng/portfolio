dc2dc2b443dd5c29fa4e293c4fa5fa1327e9d6c5
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2024-02-20 18:08:23 +0100

    exp home back

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def download_pdf
     send_file "#{Rails.root}/public/cv.pdf", type: "application/pdf", x_sendfile: true
   end
+
+  def home
+    @experiences = Experience.all
+  end
+
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -6,4 +6,5 @@ class Experience < ApplicationRecord
   validates :post, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
 end
