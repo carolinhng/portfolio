c5bf5ff0cd1e142bd01258f09099a8243d3df21d
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2024-03-01 17:46:41 +0100

    mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,12 +1,11 @@
 class MessageMailer < ApplicationMailer
 
-  default from: "caroline.ng.pro@hotmail.com"
-
+  default from: 'caroline.ng.pro@hotmail.com'
 
   # rajouter paramètre message si envoi après création du message
-  def new_message
-    @message = params[:message]
-    mail(to: "caroline.ng.pro@hotmail.com", subject: "Nouveau message du portfolio de #{@message.first_name}")
+  def new_message(message)
+    @message = message
+    mail(to: 'caroline.ng.pro@hotmail.com', subject: "Nouveau message du portfolio")
   end
 
 end
